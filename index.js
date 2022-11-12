d62882ccc9d5156db71288907b689582a33a18e2
Lucas Henrique <107213211+lucashenriq02@users.noreply.github.com>
2022-11-12 22:46:28 +0000

    Rota soma criada e ajustes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const routes = require('express').Router();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
   res.send(`
@@ -18,7 +20,7 @@ app.get('/', (req, res) => {
                 </a></button>
 
                 <button type="submit"><a href='https://Trabalho-2-Nuvem.lucasveiga19.repl.co/subtracao'> 
-                Subtação
+                Subtração
                 </a></button>
 
                 <button type="submit"><a href='https://Trabalho-2-Nuvem.lucasveiga19.repl.co/multiplicacao'> 
@@ -53,6 +55,42 @@ app.get('/', (req, res) => {
         </html> 
       `);
 });
+
+                  //soma
+app.get('/soma', (req, res) => {
+  res.send(`
+           <!DOCTYPE html>
+           <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Somando Valores</h1>
+                <h2>Digite dois numeros para somar<h2>
+              <form action="soma" method="post">
+                
+                <input type="number" name="a" value="10" />
+                <p>+</p>
+                <input type="number" name="b" value="20" />
+                <input type="submit" value="Submit"/>
+              </form>
+            </body>
+        </html> 
+      `);
+});
+
+app.post('/soma', function (req, res) {
+  var body = req.body;
+  var resultado = soma(body.a, body.b);
+  
+  res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
+});
+
+function soma(a, b) {
+  var aInt = parseInt(a);
+  var bInt = parseInt(b);
+  return aInt + bInt;
+}
 
 app.listen(3000, () => {
   console.log('server started');
--- soma.js
+++
@@ -1,6 +0,0 @@
-function soma(A = required(), B = required()) {
-  return A + B;
-}
-
-console.log(soma(1, 2));
-console.log(soma(1)); 
